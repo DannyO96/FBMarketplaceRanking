58e595ef4a39a930216389dc5623e8b9a3a8c48e
git <dannyoconnor1810@gmail.com>
2022-11-07 19:07:23 +0000

    started creating len and getitem methods for the Products.csv

--- create_pt_dataset.py
+++ create_pt_dataset.py
@@ -1,12 +1,36 @@
-
+import pandas as pd
+import PIL
+from torch.utils.data import Dataset, DataLoader
 
 
 
 class CreatePTDataset(Dataset):
-    pass
+    """
+    This class inherits from torch.utils.data.Dataset
+    
+    """
+    def __innit__(self):
+        super().__init__()
+        self.data = pd.read_csv('/my.secrets.data/Products.csv')
 
+    def __getitem__(self, index):
+        item = self.data.iloc[index]
+        id = item[2]
+        product_name = item[3]
+        category = item[4]
+        product_description = item[5]
+        price = item[6]
+        location = item[7]
+        url = item[8]
+        page_id = item[9]
+        create_time = item[10]
+        label = item[-1]
+        features = [id, product_name, product_description, category, price]
+        other_info = [location, url, page_id, create_time]
+        return {label, features, other_info}
 
-
+    def __len__(self):
+        return len(self.data)
 
 
 """
