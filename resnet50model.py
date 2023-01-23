cbbbdcb95759c8dbfe3787cc3ec2f93b03299010
git <dannyoconnor1810@gmail.com>
2023-01-23 19:32:58 +0000

    added docstrings and tested changing learning rate

--- resnet50model.py
+++ resnet50model.py
@@ -37,14 +37,14 @@ class ImageClassifier(torch.nn.Module):
 def train(model, epochs = 1):
     """
     Function to map the training loop for the model
-    Args: model 
-        : epochs
+    Args: model = model used to predict the category of images specified in self.main of the init
+        : epochs = total number of iterations of all the training data in one cycle for training
     Returns: not yet but will return trained model
         
     """
     print(model.resnet50)
     batch_idx = 0
-    lr = 0.001
+    lr = 0.00001
     loss = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr==lr)
     losses = []
@@ -71,7 +71,7 @@ def train(model, epochs = 1):
             print('-'*20)
             writer.add_scalar('Loss', l.item(), batch_idx)
 
-    writer.add_scalar('Losses', losses, batch_idx)
+    writer.add_scalar('Losses', losses)
     torch.save(model.state_dict(), 'resnet50.pt')
     with open('my.secrets.data/image_decoder.pkl', 'wb') as f:
         pickle.dump(dataset.decoder, f)
